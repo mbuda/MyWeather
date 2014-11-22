a7674b3572ac6948ad6bf68d1bb79d2348e6f2fb
Marta Buda <bmarta1991@o2.pl>
2014-11-22 10:13:23 +0100

    add #wind_direction to weather class

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -23,6 +23,31 @@ class Weather
     @ow['wind']['speed'].to_s + 'm/s'
   end
 
+  def wind_direction
+    deg = @ow['wind']['deg']
+
+    case deg
+    when 337.5..360, 0...22.5
+      'N'
+    when 22.5...67.5
+      'NE'
+    when 67.5...112.5
+      'E'
+    when 112.5...157.5
+      'SE'
+    when 157.5...202.5
+      'S'
+    when 202.5...247.5
+      'SW'
+    when 247.5...292.5
+      'W'
+    when 292.5...337.5
+      'NW'
+    else
+      'error'
+    end
+  end
+
   private
   def conf
     AppConfig.last
